8bc1ee4ed3aaffc7f54750ac9f4ee93bee92222d
Alex Luong <alex@alexluong.com>
2019-10-14 00:34:23 -0500

    feat(netlify): add response token for Twitter callback url

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,19 +1,38 @@
+const crypto = require("crypto");
+
+function createCrcResponseToken(crcToken) {
+  const hmac = crypto
+    .createHmac("sha256", process.env.TWITTER_CONSUMER_SECRET)
+    .update(crcToken)
+    .digest("base64");
+
+  return `sha256=${hmac}`;
+}
+
 function getHandler(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      method: "GET"
-    })
-  });
+  const crcToken = event.queryStringParameters.crc_token;
+
+  if (crcToken) {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({
+        response_token: createCrcResponseToken(crcToken)
+      })
+    });
+  } else {
+    callback(null, {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Error: crc_token missing from request."
+      })
+    });
+  }
 }
 
 function postHandler(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      method: "POST"
-    })
-  });
+  const body = JSON.parse(event.body);
+  console.log(body);
+  callback(null, { statusCode: 200 });
 }
 
 exports.handler = (event, context, callback) => {
